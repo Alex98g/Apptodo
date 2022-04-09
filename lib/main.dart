3c8efdd62a3a635a86d5bd05d4f4398785b0a9c1
Alex98g <lexes98@mail.ru>
2022-04-09 13:25:35 +1000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './providers/task.dart';
+import './screens/homepage.dart';
+
+void main() => runApp(ToDoListApp());
+
+class ToDoListApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => TaskProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'To Do List',
+        home: Homepage(),
+      ),
+    );
+  }
+}
